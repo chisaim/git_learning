c495d80cb8c869b893cb727d9bef46934ac43466
余晓波 <yu.xb@topcheer.com>
2019-11-27 15:36:05 +0800

    marge commit select
    edit message rename file name
    
    add select 12-22-23
    
    add select 12-23-34

---
+++ select1.sql
@@ -0,0 +1,4 @@
+select * from select11;
+select * from select12;
+select * from select13;
+select * from select14;
---
+++ select2.sql
@@ -0,0 +1,4 @@
+select * from select21;
+select * from select22;
+select * from select23;
+select * from select24;
