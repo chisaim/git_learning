7006f0684e182914a24e178bc39796e25e50419a
余晓波 <yu.xb@topcheer.com>
2019-11-28 11:50:56 +0800

    add file select3.sql

---
+++ select3.sql
@@ -0,0 +1,4 @@
+select * from select31;
+select * from select32;
+-- select * from select33;
+-- select * from select34;
---
+++ select4.sql
@@ -0,0 +1,4 @@
+select * from select41;
+select * from select42;
+-- select * from select43;
+-- select * from select44;
